c29bc28ef5658ab0861722852d1335e0fc8bd7de
sbot19 <92232719+sbot19@users.noreply.github.com>
2022-01-03 22:33:31 -0500

    Moved the backgrounds folder to the PUBLIC folder and modified the background component

--- src/Background.js
+++ src/Background.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Background(props) {
   return (
     <img
-      src={require(`./images/backgrounds/${props.background}.gif`)}
+      src={`/images/backgrounds/${props.background}.gif`}
       alt="Current weather background"
     />
   );
