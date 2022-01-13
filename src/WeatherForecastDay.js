9425e0c5784b599160a8aa0afc082e08d1356c57
sbot19 <92232719+sbot19@users.noreply.github.com>
2022-01-12 20:42:45 -0500

    Removed temperature conversion

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,61 +2,6 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
-  function forecastDay() {
-    function day() {
-      let date = new Date(props.data.dt * 1000);
-      let day = date.getDay();
-
-      let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-      return days[day];
-    }
-
-    let highTempCelsius = Math.round(props.data.temp.max);
-    let highTempFahrenheit = Math.round((props.data.temp.max * 9) / 5 + 32);
-
-    let lowTempCelsius = Math.round(props.data.temp.min);
-    let lowTempFahrenheit = Math.round((props.data.temp.min * 9) / 5 + 32);
-
-    if (props.unit === "metric") {
-      return (
-        <div>
-          <div className="forecast-day">
-            {day()}
-            <WeatherIcon code={props.data.weather[0].icon} size={36} />
-            <div className="forecast-day-temps">
-              <div className="forecast-high-temp">{highTempCelsius}˚C</div>
-              <div className="forecast-low-temp">{lowTempCelsius}˚C</div>
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <div className="forecast-day">
-            {day()}
-            <WeatherIcon code={props.data.weather[0].icon} size={36} />
-            <div className="forecast-day-temps">
-              <div className="forecast-high-temp">{highTempFahrenheit}˚F</div>
-              <div className="forecast-low-temp">{lowTempFahrenheit}˚F</div>
-            </div>
-          </div>
-        </div>
-      );
-    }
-  }
-  return <div>{forecastDay()}</div>;
-}
-
-/* 
-    if (props.unit === "metric") {
-      return `${lowTempCelsius} ˚C`;
-    } else {
-      return `${lowTempFahrenheit} ˚F`;
-    }
-  }
-
-  
   function highTemp() {
     let temperature = Math.round(props.data.temp.max);
     return `${temperature}˚`;
@@ -65,8 +10,15 @@ export default function WeatherForecastDay(props) {
   function lowTemp() {
     let temperature = Math.round(props.data.temp.min);
     return `${temperature}˚`;
-  } 
+  }
 
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    return days[day];
+  }
 
   return (
     <div>
@@ -74,10 +26,10 @@ export default function WeatherForecastDay(props) {
         {day()}
         <WeatherIcon code={props.data.weather[0].icon} size={36} />
         <div className="forecast-day-temps">
-          <div className="forecast-high-temp">{maxTemp()}</div>
-          <div className="forecast-low-temp">{minTemp()}</div>
+          <div className="forecast-high-temp">{highTemp()}</div>
+          <div className="forecast-low-temp">{lowTemp()}</div>
         </div>
       </div>
     </div>
   );
-  */
+}
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,72 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <span>
-        <span className="WeatherTemperature">
-          <div className="row">
-            <div className="col-6 pe-1">
-              <span className="temperature">{Math.round(props.celsius)}</span>{" "}
-              <span className="unit">
-                <button>˚C</button> <button onClick={showFahrenheit}>˚F</button>
-              </span>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>Feels like: {props.feelsLike}˚C</li>
-                <li>
-                  High: {props.highTemp}˚C | Low: {props.lowTemp}˚C
-                </li>
-                <li>Humidity: {props.humidity}%</li>
-                <li>Wind: {props.wind} km/h</li>
-              </ul>
-            </div>
+  return (
+    <span>
+      <span className="WeatherTemperature">
+        <div className="row">
+          <div className="col-6 pe-1">
+            <span className="temperature">{Math.round(props.celsius)}</span>{" "}
+            <span className="unit">˚C</span>
           </div>
-        </span>
-      </span>
-    );
-  } else {
-    return (
-      <span>
-        <div className="WeatherTemperature">
-          <div className="row">
-            <div className="col-6 pe-1">
-              <span className="temperature">
-                {Math.round((props.celsius * 9) / 5 + 32)}
-              </span>{" "}
-              <span className="unit">
-                {" "}
-                <button onClick={showCelsius}>˚C</button> <button>˚F</button>
-              </span>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>
-                  Feels like: {Math.round((props.feelsLike * 9) / 5 + 32)}˚F
-                </li>
-                <li>
-                  High: {Math.round(props.highTemp * (9 / 5) + 32)}˚F | Low:{" "}
-                  {Math.round(props.lowTemp * (9 / 5) + 32)}˚F
-                </li>
-                <li>Humidity: {props.humidity}%</li>
-                <li>Wind: {Math.round(props.wind*0.62137)} mph</li>
-              </ul>
-            </div>
+          <div className="col-6">
+            <ul>
+              <li>Feels like: {props.feelsLike}˚C</li>
+              <li>
+                High: {props.highTemp}˚C | Low: {props.lowTemp}˚C
+              </li>
+              <li>Humidity: {props.humidity}%</li>
+              <li>Wind: {props.wind} km/h</li>
+            </ul>
           </div>
         </div>
       </span>
-    );
-  }
+    </span>
+  );
 }
