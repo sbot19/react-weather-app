ad7558ce5b4fa24667a46d0cc995e09073609c1e
sbot19 <92232719+sbot19@users.noreply.github.com>
2022-01-12 22:51:44 -0500

    Made change to the WeatherIcon component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,23 @@ import Weather from "./Weather";
 import "./Weather.css";
 import "./App.css";
 
-/* import "./App.css"; */
-
 export default function App() {
   return (
-    <body>
-      <div className="container">
-        <div className="weather-app">
-          <Weather defaultCity="New York" />
-          <footer className="open-source text-center">
-            This app was coded by Scarleth Bernabe and is {""}
-            <a
-              href="https://github.com/sbot19/react-weather-app"
-              target="_blank"
-              rel="noreferrer"
-              className="open-source-link"
-            >
-              open-sourced on GitHub
-            </a>
-          </footer>
-        </div>
+    <div className="container">
+      <div className="weather-app">
+        <Weather defaultCity="New York" />
+        <footer className="open-source text-center">
+          This app was coded by Scarleth Bernabe and is {""}
+          <a
+            href="https://github.com/sbot19/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+            className="open-source-link"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
-    </body>
+    </div>
   );
 }
